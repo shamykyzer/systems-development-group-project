eb9c1f01db3970765fe009eeea4ebd907538de19
Ollie Churchley <olliechurchley@gmail.com>
2026-02-23 19:44:31 +0000

    Revert "feat: UI redesign overhaul + cleaner code structure under /src now splitting both backend & frontend"

---
+++ pinkcafe/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import LandingPagePanel from '../components/LandingPagePanel';
+
+
+function Home() {
+  return (
+<div>
+    <NavBar />
+    <LandingPagePanel />
+</div>
+  );
+}
+export default Home;
